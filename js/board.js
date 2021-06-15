34dec2c7fb6d3dc469064421a3fde805056578d7
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-06-15 22:17:20 +0200

    board JS function showTasks

---
+++ js/board.js
@@ -0,0 +1,51 @@
+let tasks = [{
+
+    'id': 0,
+    'title': 'Test1',
+    'category': 'open'
+
+},{
+
+    'id': 1,
+    'title': 'Test2',
+    'category': 'open'
+
+},{
+
+    'id': 2,
+    'title': 'Test2',
+    'category': 'closed'
+
+
+}];
+
+function showTasks(){
+
+    let open = tasks.filter(t => t['category'] == 'open');
+
+    document.getElementById('open').innerHTML = '';
+
+    for (let i = 0; i < open.length; i++) {
+        const element = open[i];
+        document.getElementById('open').innerHTML += generateToDoHTML(element);
+        
+    }
+
+    let closed = tasks.filter(t => t['category'] == 'closed');
+
+    document.getElementById('closed').innerHTML = '';
+
+    for (let i = 0; index < closed.length; i++) {
+        const element = closed[i];
+        document.getElementById('closed').innerHTML += generateToDoHTML(element);
+        
+    }
+
+}
+
+function generateToDoHTML(element){
+
+    return `<div class="taskContainer">${element['title']}</div>`;
+
+
+}
