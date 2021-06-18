27e3c83ef93aff683ae055dd142bcd7700e75f35
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-06-18 19:01:01 +0200

    js board changes

--- js/board.js
+++ js/board.js
@@ -32,6 +32,27 @@ function updateHTML(){
         
     }
 
+    let inProgress = toDo.filter(t => t['category'] == 'inProgress');
+
+    document.getElementById('inProgress').innerHTML = '';
+
+    for (let i = 0; i < inProgress.length; i++) {
+        const element = inProgress[i];
+        document.getElementById('inProgress').innerHTML += generateToDoHTML(element);
+        
+    }
+
+    let testing = toDo.filter(t => t['category'] == 'testing');
+
+    document.getElementById('testing').innerHTML = '';
+
+    for (let i = 0; i < testing.length; i++) {
+        const element = testing[i];
+        document.getElementById('testing').innerHTML += generateToDoHTML(element);
+        
+    }
+
+
     let closed = toDo.filter(t => t['category'] == 'closed');
 
     document.getElementById('closed').innerHTML = '';
