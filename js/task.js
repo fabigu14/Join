0ee295716ddef047629b550116fc9be191c14736
Fabian Gurth <fabigurth@googlemail.com>
2021-06-18 22:07:50 +0200

    create task

--- js/task.js
+++ js/task.js
@@ -1,3 +1,25 @@
-function submit(){
-    
+inputFields = ['title', 'due_date', 'category', 'urgency', 'description'];
+task = {}
+usersAssigned = [];
+
+function createTask(){
+    inputFields.forEach(inputField => {
+       let currentField = document.getElementById(inputField);
+       let inputValue = currentField.value;
+       task[inputField] = inputValue;
+    });
+    addToTasks();
+    clearInput();
+}
+
+function addToTasks(){
+    tasks.push(task);
+}
+
+function clearInput(){
+    inputFields.forEach(inputField => {
+        let currentField = document.getElementById(inputField);
+        currentField.value = '';
+     });
+     task = {};
 }
