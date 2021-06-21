0f3c7855cb3279e4cb5c08ce0e3003cacde93a0f
MustafaGuer <mustafaguer@outlook.com>
2021-06-21 04:22:57 +0200

    added set user function

--- js/login-submit.js
+++ js/login-submit.js
@@ -6,6 +6,7 @@
 //     submit.classList.toggle('d-none');
 // }
 
+
 function goToSubmit() {
     let login = document.getElementById('loginContainer');
     let submit = document.getElementById('submitContainer');
@@ -18,3 +19,28 @@ function goToLogin() {
     document.getElementById('loginContainer').style = 'transform: translateY(0%)';
     document.getElementById('submitContainer').style = 'transform: translateY(100%)';
 }
+
+function addUser() {
+    let name = document.getElementById('nameInput');
+    let username = document.getElementById('usernameInput');
+    let email = document.getElementById('emailInput');
+    let password = document.getElementById('passwordInput');
+
+    let user = {
+        'name': name.value,
+        'username': username.value,
+        'email': email.value,
+        'password': password.value
+    };
+    name.value = '';
+    username.value = '';
+    email.value = '';
+    password.value = '';
+
+    users.push(user);
+    backend.setItem('users', JSON.stringify(users));
+}
+
+// function deleteUser(name) {
+//     backend.deleteItem('users');
+//   }
