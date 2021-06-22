67293302014d1296edd527b75286c6a1ee448c19
Fabian Gurth <fabigurth@googlemail.com>
2021-06-22 11:47:08 +0200

    task.js

--- js/task.js
+++ js/task.js
@@ -1,41 +1,57 @@
-inputFields = ['title', 'due_date', 'category', 'urgency', 'description'];
+inputFields = ['title', 'due_date', 'category_value', 'urgency_value', 'description'];
 task = {};
 usersAssigned = [];
+dropdownIsShowing = false;
 
-function createTask(){
+function createTask() {
     inputFields.forEach(inputField => {
-       let currentField = document.getElementById(inputField);
-       let inputValue = currentField.value;
-       task[inputField] = inputValue;
+        let currentField = document.getElementById(inputField);
+        let inputValue = currentField.value;
+        task[inputField] = inputValue;
     });
-    
+
     addToTasks();
     clearInput();
 }
 
-function addToTasks(){
+function addToTasks() {
     tasks.push(task);
     saveTasksToServer();
 }
 
-function clearInput(){
+function clearInput() {
     inputFields.forEach(inputField => {
         let currentField = document.getElementById(inputField);
         currentField.value = '';
-     });
-     task = {};
+    });
+    task = {};
 }
 
 function showDropdown(id) {
     document.getElementById(id + '_dropdown').classList.remove('d-none');
     document.getElementById(id).classList.add('input-radius');
+    dropdownIsShowing = true;
+    showOverlay();
 }
 
-function setSelection(selection, inputId, dropdown) {
+function showOverlay(){
+    document.getElementById('overlay').classList.remove('d-none');
+}
+
+function hideOverlay(){
+    document.getElementById('overlay').classList.add('d-none');
+}
+
+function setSelection(selection, inputId) {
     document.getElementById(inputId).value = selection;
-    hideDropdown(dropdown);
+    hideDropdown();
 }
 
-function hideDropdown(dropdown) {
-    document.getElementById(dropdown).classList.add('d-none');
+function hideDropdown() {
+    if (dropdownIsShowing) {
+        document.getElementById('category_dropdown').classList.add('d-none');
+        document.getElementById('urgency_dropdown').classList.add('d-none');
+        hideOverlay();
+        dropdownIsShowing = false;
+    }
 }
