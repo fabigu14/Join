f7ac92be34cc93da2c491f186ec105e5c6104be6
MustafaGuer <mustafaguer@outlook.com>
2021-06-24 15:37:37 +0200

    set backlog back

--- js/backlog.js
+++ js/backlog.js
@@ -6,42 +6,30 @@ async function initBacklog() {
 
 function clearBacklog() {
     let backlogContent = document.getElementById('backlogContent');
-
     backlogContent.innerHTML = '';
-
 }
 
 function updateBacklog() {
     clearBacklog();
 
-    let backlogContent = document.getElementById('backlogContent');
     for (let i = 0; i < tasks.length; i++) {
         const task = tasks[i];
-        backlogContent.innerHTML += renderBacklogRows(task, i);
-        renderBacklogRowUsers(i);
-        checkCategory(i);
+        for (let j = 0; j < task['assigned_users'].length; j++) {
+            const user = task['assigned_users'][j];
+            backlogContent.innerHTML += renderBacklogRows(task, user, i);
+            checkCategory(i);
+        }
     }
 }
 
-function renderBacklogRowUsers(i) {
-    let user = tasks[i]['assigned_users'];
-
-    for (let j = 0; j < user.length; j++) {
-        user = user[j];
-        document.getElementById('backlogUser_' + i).innerHTML += `
-            <img id="blUserImg_${i}" class="blUserImg" src="../${user['img']}">
-            <div class="blUserData">
-                <span id="blUserName${i}">${user['name']}</span>
-                <a id="blUserEmail_${i}" href="#">${user['email']}</a>
-            </div>
-        `;
-    }
-}
-
-function renderBacklogRows(task, i) {
+function renderBacklogRows(task, user, i) {
     return `<div id="backlogRows_${i}" class="backlogRows">
-                    <div id="backlogUser_${i}" class="backlogUser">
-                        
+                    <div class="backlogUser">
+                        <img id="blUserImg_${i}" class="blUserImg" src="../${user['img']}">
+                        <div class="blUserData">
+                            <span id="blUserName${i}">${user['name']}</span>
+                            <a id="blUserEmail_${i}" href="#">${user['email']}</a>
+                        </div>
                     </div>
                         <div id="blCategory_${i}" class="blCategories"><span class="hiddenText">Category:</span><span>${task['category_value']}</span></div>
                         <div id="blDetail_${i}" class="blDetails"><span class="hiddenText">Description:</span><span>${task['description']}</span></div>
@@ -67,7 +55,7 @@ function checkCategory(i) {
         default:
             result = 'default-bl';
     }
-
+    
     drawBorder(result, i);
 }
 
