297d4e691244b0d1b1b8ae7ad0cc9a360be919df
Fabian Gurth <fabigurth@googlemail.com>
2021-06-28 23:06:32 +0200

    infoBox resolved

--- js/board.js
+++ js/board.js
@@ -1,6 +1,5 @@
 let currentDraggedElement;
 let currentDraggedCategory;
-let containerId = 0;
 
 async function initboard(){
     
@@ -27,18 +26,15 @@ function updateHTML(){
     let done = tasks.filter(t => t['state'] == 'done');
     update('done', done);
     
-    containerId = 0;
+    
 }
 
 function update(containerID, array){
     
     document.getElementById(containerID).innerHTML = '';
     for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+    let element = array[i];
     document.getElementById(containerID).innerHTML += generateToDoHTML(element,i);
-
-    console.log(containerId);
-    containerId++;
     
     }
 
@@ -47,28 +43,13 @@ function update(containerID, array){
 function generateToDoHTML(element, i){
     
 
-    console.log(containerId);
-    return `<div draggable="true" onclick="openContainer(${i})" ondragstart="startdragging(${i}, '${element['state']}')" class="taskContainer">${element['title']}</div>
+    console.log(element);
+    return `<div draggable="true" onclick="openContainer('${element['title']}', '${element['description']}', '${element['due_date']}')" ondragstart="startdragging(${i}, '${element['state']}')" class="taskContainer">${element['title']}</div>
             
 
-            <div id="openContainer${containerId}" class="openContainer d-none">
-            <div class="infoBox">
-            <div class="headlinebox">
-            <div class="headline"><h2>${tasks[containerId]['title']}</h2></div>
-            <div onclick="closeContainer()" class="image"><img src="/img/x-mark-16.png"></div>
-            </div>
-            <div class="descriptionContainer">${tasks[containerId]['description']}</div>
-            <div class="date-section"><p>Deadline: ${tasks[containerId]['due_date']}</p></div>
+            <div id="openContainer" class="openContainer d-none">
             
-            <div class="assigned-container">
-            <div class="assignedUser"><p>Assigned To:</p></div>
-            <div class="profileImage">
-            <img src="../img/fabi.jpg" alt="profile-img">
-            </div>
             
-            <div onclick="deletetasks()" class="deleteButton">
-            <button>Delete</button>
-            </div>
             </div>
       
     `;
@@ -109,9 +90,29 @@ function removehighlight(id){
     document.getElementById(id).classList.remove('grey-box-highlight');
 
 }
-function openContainer(id){
-    console.log(id);
-    document.getElementById(`openContainer` + id).classList.remove('d-none');
+function openContainer(title, description, due_date){
+    console.log(title);
+    let container = document.getElementById(`openContainer`);
+
+    container.innerHTML = `<div class="infoBox">
+    <div class="headlinebox">
+    <div class="headline"><h2>${title}</h2></div>
+    <div onclick="closeContainer()" class="image"><img src="/img/x-mark-16.png"></div>
+    </div>
+    <div class="descriptionContainer">${description}</div>
+    <div class="date-section"><p>Deadline: ${due_date}</p></div>
+    
+    <div class="assigned-container">
+    <div class="assignedUser"><p>Assigned To:</p></div>
+    <div class="profileImage">
+    <img src="../img/fabi.jpg" alt="profile-img">
+    </div>
+    
+    <div onclick="deletetasks()" class="deleteButton">
+    <button>Delete</button>
+    </div>`
+    container.classList.remove('d-none');
+
         
 }
 
